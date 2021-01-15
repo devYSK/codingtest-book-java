ee973b1109e6caa953bea87bbdb1dd6b7fabe9c8
ysk <kim206gh@naver.com>
2021-01-15 16:24:45 +0900

    전보 문제 다익스트라 적용 풀이

--- src/chapter9/전보.java
+++ src/chapter9/전보.java
@@ -13,47 +13,44 @@ public class 전보 {
 
     private static int[] table = new int[30001];
 
-    private static ArrayList<Node[]> graph = new ArrayList<>();
+    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 
-    public static void solution() {
-
+    public static void solution(int start) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
 
-        pq.offer(new Node(c, 0));
-
-        table[c] = 0;
+        pq.offer(new Node(start, 0));
+        table[start] = 0;
 
         while (!pq.isEmpty()) {
             Node node = pq.poll();
 
-            int distance = node.getDistance();
-            int nowIdx = node.getIndex();
+            int currentDistance = node.getDistance();
+            int currentIdx = node.getIndex();
 
-            if (table[nowIdx] < distance)
-                continue;
+            if (table[currentIdx] < currentDistance) continue;
 
-            for (int i = 0; i < graph.get(nowIdx).length; i++) {
-                int cost = table[nowIdx] + graph.get(nowIdx)[i].getDistance();
+            for (int i = 0; i< graph.get(currentIdx).size(); i++) {
+                int otherCost = table[currentIdx] + graph.get(currentIdx).get(i).getDistance();
+                int otherIdx = graph.get(currentIdx).get(i).getIndex();
 
-                if (cost < table[graph.get(nowIdx)[i].getIndex()]) {
-                    table[graph.get(nowIdx)[i].getIndex()] = cost;
-                    pq.offer(new Node(graph.get(nowIdx)[i].getIndex(), cost));
+                if (otherCost < table[otherIdx]) {
+                    table[otherIdx] = otherCost;
+                    pq.offer(new Node(otherIdx, otherCost));
                 }
             }
 
         }
 
-
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        c = sc.nextInt();
+        n = sc.nextInt(); // 도시의 개수
+        m = sc.nextInt(); // 통로의 개수
+        c = sc.nextInt(); // 메시지를 보내고자 하는 도시 C
 
         for (int i = 0; i <= n; i++) {
-            graph.add(new Node[n]);
+            graph.add(new ArrayList<Node>());
         }
 
         for (int i = 0; i < m; i++) {
@@ -61,28 +58,25 @@ public class 전보 {
             int y = sc.nextInt();
             int z = sc.nextInt();
 
-            // x번 노드 -> y번 노드 까지 거리 = z
-            graph.get(x)[i] = new Node(y, z);
+            graph.get(x).add(new Node(y, z));
         }
 
         Arrays.fill(table, INF);
 
-// 다익스트라 알고리즘을 수행
-        solution();
+        solution(c); // 메시지를 보내고자 하는 도시부터 다익스트라 알고리즘 수행
 
-        // 도달할 수 있는 노드의 개수
         int count = 0;
-        // 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
-        int maxDistance = 0;
+
+        int maxDistance = 0; // 도달할 수 있는 노드중에서 가장 멀리 있는 노드와의 최단 거리
+
         for (int i = 1; i <= n; i++) {
-            // 도달할 수 있는 노드인 경우
             if (table[i] != INF) {
                 count += 1;
                 maxDistance = Math.max(maxDistance, table[i]);
             }
         }
 
-        // 시작 노드는 제외해야 하므로 count - 1을 출력
+        // 시작 노드는 제외하므로 count에서 -1
         System.out.println((count - 1) + " " + maxDistance);
 
 
