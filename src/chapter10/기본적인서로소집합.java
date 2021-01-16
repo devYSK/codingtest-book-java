89f513672dfa11e450fcbb6313c86598aec2a2b9
ysk <kim206gh@naver.com>
2021-01-16 23:01:17 +0900

    기본적 서로소 집합 알고리즘

--- src/chapter10/기본적인서로소집합.java
+++ src/chapter10/기본적인서로소집합.java
@@ -5,35 +5,21 @@ import java.util.Scanner;
 public class 기본적인서로소집합 {
 
     private static int v, e;
+    private static int[] parent = new int[100001];
 
-    private static int[] parent = new int[1000001];// 부모 테이블
+    public static int findParent(int x) {
 
-//    private static int findParent(int x) {
-//        return x == parent[x] ? // 자기 자신이 루트 노드이면(부모노드)
-//                x : findParent(parent[x]);
-//    }
-//
-
-    //개선한 find
-    private static int findParent(int x) {
-//        if (parent[x] == x)
-//            parent[x] = findParent(x);
-
-        if (x == parent[x])
-            return x;
-
-        return parent[x] = findParent(parent[x]);
+        if (x == parent[x]) return x;
+        return findParent(parent[x]);
     }
 
-
-
-    private static void unionParent(int a, int b) {
+    public static void unionParent(int a, int b) {
         a = findParent(a);
         b = findParent(b);
-
         if (a < b) parent[b] = a;
         else parent[a] = b;
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -41,29 +27,30 @@ public class 기본적인서로소집합 {
         v = sc.nextInt();
         e = sc.nextInt();
 
+        // 부모 테이블상에서, 부모를 자기 자신으로 초기화
         for (int i = 1; i <= v; i++) {
             parent[i] = i;
         }
 
+        // Union 연산을 각각 수행
         for (int i = 0; i < e; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             unionParent(a, b);
         }
 
-        System.out.print("각 원소가 속한 집합 : ");
-
-        for (int i = 1; i <= v; i++)
+        // 각 원소가 속한 집합 출력하기
+        System.out.print("각 원소가 속한 집합: ");
+        for (int i = 1; i <= v; i++) {
             System.out.print(findParent(i) + " ");
-
+        }
         System.out.println();
 
-        System.out.print("부모 테이블 : ");
-
-        for (int i = 1; i<= v; i++)
+        // 부모 테이블 내용 출력하기
+        System.out.print("부모 테이블: ");
+        for (int i = 1; i <= v; i++) {
             System.out.print(parent[i] + " ");
-
-
+        }
         System.out.println();
 
     }
