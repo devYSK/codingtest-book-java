4cb0dab63a329e8d3a8c6cbd8e4d6ae6c6be1e74
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-01-23 23:12:35 +0900

    위상정렬 학습

--- src/chapter10/위상정렬.java
+++ src/chapter10/위상정렬.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 위상 정렬은 순서가 정해져 있는 일련의 작업을
+ * 차례대로 수행해야 할 때 사용할 수 있는 알고리즘이다. 조금 더 이론적으로 설명하자면,
+ * 위상 정렬이란 빙향 그래프의 모든 노드를 ‘방향성에 거스르지 않도록 순서 대로 나열하는 것'이다
+ */
 public class 위상정렬 {
 
     private static int v, e;
