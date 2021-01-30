c63f221a44822061bf9ea24c73003fe816f3c483
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-01-30 18:50:54 +0900

    모험가길드 풀이

--- src/chapter11/모험가길드.java
+++ src/chapter11/모험가길드.java
@@ -1,9 +1,39 @@
 package chapter11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class 모험가길드 {
+
+    private static int n;
+    private static List<Integer> groups = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            groups.add(sc.nextInt());
+        }
+
+        Collections.sort(groups);
+
+        int groupCnt = 0;
+        int currentGroupCnt = 0;
+
+        for (int i = 0; i < groups.size(); i++) {
+            currentGroupCnt += 1;
+
+            if (currentGroupCnt >= groups.get(i)) {
+                groupCnt +=1;
+                currentGroupCnt = 0;
+            }
+
+        }
+        System.out.println(groupCnt);
     }
 }
